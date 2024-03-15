09b00c0fa4afcab10fb778cbda88002e854ddc3a
Ian Stadler <istadler@semios.com>
2024-03-15 14:33:28 -0700

    drivers: adc: ambiq: Apollo3 ADC support
    Added support for the ADC peripheral on the Ambiq Apollo3 MCU
    
    Signed-off-by: Ian Stadler <istadler@semios.com>

---
+++ include/zephyr/dt-bindings/adc/ambiq_adc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Semios Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_AMBIQ_ADC_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_AMBIQ_ADC_H_
+
+/* Valid pins for differential ADC inputs */
+#define AMBIQ_ADC_N_P12 12
+#define AMBIQ_ADC_P_P13 13
+#define AMBIQ_ADC_P_P14 14
+#define AMBIQ_ADC_N_P15 15
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_AMBIQ_ADC_H_ */
